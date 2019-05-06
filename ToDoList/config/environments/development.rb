9fdc2efc3d70528f7ff09d40baa1ef63a844e4b4
zorek9502 <zorek_ltc@hotmail.com>
2019-05-06 18:46:14 -0500

    Changed some configuratios Replace the gem factory_bot for factory_bot_rails Sign In test

--- ToDoList/config/environments/development.rb
+++ ToDoList/config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
+  config.active_job.queue_adapter = :sidekiq
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -35,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
--- ToDoList/spec/factories/user.rb
+++ ToDoList/spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
-  factory :user, class: User do
+  factory :user do
     username { Faker::Name.name }
     email { Faker::Internet.email }
     description { Faker::TvShows::GameOfThrones.quote }
+    password { "Holamundo123" }
   end
 end
